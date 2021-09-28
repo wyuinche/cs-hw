b63cf6d02dcafcbd7cea1f38fd2d1a89566891b6
wyuinche <wyuinche.mido@gmail.com>
2021-09-28 14:19:02 +0900

    Update license headers

--- 2019-02/데이터베이스개론/DB_project/source_code_files/music_main.php
+++ 2019-02/데이터베이스개론/DB_project/source_code_files/music_main.php
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2019  Oracle Corporation and/or its affiliates
+Copyright (C) 2019 wyuinche
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
